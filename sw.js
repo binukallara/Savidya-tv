2ebd9b30180848d7880ca76a6ce61c15379677bc
binukallara <binukallara@gmail.com>
2026-08-10 09:55:30 +0800

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,21 +1,39 @@
+const CACHE_NAME = 'savidya-tv-v3';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js'
+];
+
+// Install Event
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('savidya-tv-v2').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/sw.js'
-      ]).catch(function() { /* ignore */ });
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS_TO_CACHE).catch(function() { /* ignore */ });
     })
   );
   self.skipWaiting();
 });
 
+// Activate Event (പഴയ ക്യാഷുകൾ നീക്കം ചെയ്യുന്നു)
 self.addEventListener('activate', function(e) {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cache) {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // വേഷൻ മാറമ്പോൾ പഴയ ക്യാഷ് കളയും
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
 });
 
+// Fetch Event
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(resp) {
